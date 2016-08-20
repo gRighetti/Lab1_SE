1960a2eaed90c1c707505fabe677472e18492f92
guido righetti <guido righetti>
2016-08-19 21:03:02 -0300

    Primera prueba de los RGB con el pote para cambiar el color(funciona)

--- src/main.c
+++ src/main.c
@@ -5,9 +5,8 @@ void APP_1ms(void);
 #define INCREASE 1
 #define DECREASE 0
 
-
 uint8_t brightness = 0;
-uint16_t Value_Pote=0;
+uint16_t Value_Pote = 0;
 uint8_t Calculo;
 
 int main(void) {
@@ -18,47 +17,45 @@ int main(void) {
 
 		Value_Pote = Value_Potenciometro();
 
-		if(Value_Pote <=120){
-					rgb_setBright(RGB_ROJO,100);
-					rgb_setBright(RGB_AZUL,0);
-					rgb_setBright(RGB_VERDE, 1000/120*Value_Pote);
+		if (Value_Pote <= 120) {
+			rgb_setBright(RGB_ROJO, 100);
+			rgb_setBright(RGB_AZUL, 0);
+			rgb_setBright(RGB_VERDE, 1000 / 120 * Value_Pote);
 		}
-		if(Value_Pote>120 && Value_Pote<=260){
+		if (Value_Pote > 120 && Value_Pote <= 260) {
 
-							rgb_setBright(RGB_ROJO, 1000-((1000/140)*Value_Pote));
-							rgb_setBright(RGB_AZUL,0);
-							rgb_setBright(RGB_VERDE, 100);
-				}
-		if(Value_Pote>260 && Value_Pote<=420){
+			rgb_setBright(RGB_ROJO, 1000 - (1000 / 140 * Value_Pote));
+			rgb_setBright(RGB_AZUL, 0);
+			rgb_setBright(RGB_VERDE, 100);
+		}
+		if (Value_Pote > 260 && Value_Pote <= 420) {
 
-									rgb_setBright(LED_ROJO, 0);
-									rgb_setBright(LED_AZUL,0);
-									rgb_setBright(LED_VERDE, 100);
-						}
-		if(Value_Pote>420 && Value_Pote<=590){
+			rgb_setBright(RGB_ROJO, 0);
+			rgb_setBright(RGB_AZUL, 1000 / 140 * Value_Pote);
+			rgb_setBright(RGB_VERDE, 100);
+		}
+		if (Value_Pote > 420 && Value_Pote <= 590) {
 
-									rgb_setBright(LED_ROJO, 1000-((1000/140)*Value_Pote));
-									rgb_setBright(LED_AZUL,0);
-									rgb_setBright(LED_VERDE, 100);
-						}
-		if(Value_Pote>590 && Value_Pote<=790){
+			rgb_setBright(RGB_ROJO, 0);
+			rgb_setBright(RGB_AZUL, 100);
+			rgb_setBright(RGB_VERDE, 1000 - (1000 / 170 * Value_Pote));
+		}
+		if (Value_Pote > 590 && Value_Pote <= 790) {
 
-									rgb_setBright(LED_ROJO, 1000-((1000/140)*Value_Pote));
-									rgb_setBright(LED_AZUL,0);
-									rgb_setBright(LED_VERDE, 100);
-						}
-		if(Value_Pote>790){
+			rgb_setBright(RGB_ROJO, 1000 / 140 * Value_Pote);
+			rgb_setBright(RGB_AZUL, 100);
+			rgb_setBright(RGB_VERDE, 0);
+		}
+		if (Value_Pote > 790) {
 
-									rgb_setBright(LED_ROJO, 1000-((1000/140)*Value_Pote));
-									rgb_setBright(LED_AZUL,0);
-									rgb_setBright(LED_VERDE, 100);
-						}
-
+			rgb_setBright(RGB_ROJO, 100);
+			rgb_setBright(RGB_AZUL,1000 - (1000 / 170 * Value_Pote));
+			rgb_setBright(RGB_VERDE, 0);
+		}
 
 	}
 }
 
 void APP_1ms(void) {
-
 
 }
