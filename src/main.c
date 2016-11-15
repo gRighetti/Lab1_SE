5696cfc319575ae69c9e667d560ac441f045d4fa
guido righetti <guido righetti>
2016-11-15 20:42:50 -0300

    ahora si anda bien, listo para entregar

--- src/main.c
+++ src/main.c
@@ -16,40 +16,44 @@ int main(void) {
 	while (1) {
 
 		Value_Pote = Value_Potenciometro();
-
+// 0.12*4095=491.4
 		if (Value_Pote <= 491) {
 			rgb_setBright(RGB_ROJO, 100);
 			rgb_setBright(RGB_AZUL, 0);
-			rgb_setBright(RGB_VERDE, 100/491 * Value_Pote);
+			rgb_setBright(RGB_VERDE,(uint8_t) (( Value_Pote) * 100/491) );
 		}
 		if (Value_Pote > 491 && Value_Pote <= 1065) {
-
-			rgb_setBright(RGB_ROJO, 100 - (100/573 * (Value_Pote-492)));
+//0.26*4095=1064.7
+			rgb_setBright(RGB_ROJO,(uint8_t)( 100 - ( (Value_Pote-492) * 100/573)));
 			rgb_setBright(RGB_AZUL, 0);
 			rgb_setBright(RGB_VERDE, 100);
 		}
+//0.42*4095=1719.9
 		if (Value_Pote > 1065 && Value_Pote <= 1720) {
 
 			rgb_setBright(RGB_ROJO, 0);
-			rgb_setBright(RGB_AZUL, 100 / 655 * (Value_Pote-1066));
+			rgb_setBright(RGB_AZUL,(uint8_t) ((Value_Pote-1066)* 100 / 654)  );
 			rgb_setBright(RGB_VERDE, 100);
 		}
+//0.59*4095=2416.05
 		if (Value_Pote > 1720 && Value_Pote <= 2416) {
 
 			rgb_setBright(RGB_ROJO, 0);
 			rgb_setBright(RGB_AZUL, 100);
-			rgb_setBright(RGB_VERDE, 100 - (100/696 * (Value_Pote-1721)));
+			rgb_setBright(RGB_VERDE,(uint8_t)( 100 - ( (Value_Pote-1721) * 100/696 )));
 		}
+//0.78*4095=3194.1
 		if (Value_Pote > 2416 && Value_Pote <= 3235) {
 
-			rgb_setBright(RGB_ROJO, 100 / 819 * (Value_Pote-2417));
+			rgb_setBright(RGB_ROJO, (uint8_t) ((Value_Pote-2417)* 100 / 818 ));
 			rgb_setBright(RGB_AZUL, 100);
 			rgb_setBright(RGB_VERDE, 0);
 		}
+
 		if (Value_Pote > 3235) {
 
 			rgb_setBright(RGB_ROJO, 100);
-			rgb_setBright(RGB_AZUL,100 - (100/860 * (Value_Pote-3236)));
+			rgb_setBright(RGB_AZUL,(uint8_t) (100 - ((Value_Pote-3236)* 100/859 )));
 			rgb_setBright(RGB_VERDE, 0);
 		}
 
